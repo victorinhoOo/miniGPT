8f0fba31224b656c4f9b0745968630acb2a84cc7
Victor Duboz <dubozvictor@gmail.com>
2025-02-23 11:37:37 +0100

    feat: Initialisation des poids du modèle pour suivre une distribution normale,  création d'un dataloader pour entraîner le modèle

--- train_gpt.py
+++ train_gpt.py
@@ -29,6 +29,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # projection de sortie pour combiner les résultats de toutes les têtes
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.MINIGPT_SCALE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         # création du masque triangulaire causal qui empêche de voir les tokens futurs
@@ -89,6 +90,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.MINIGPT_SCALE_INIT = 1
     
     def forward(self, x):
         """Applique la transformation MLP sur l'entrée.
@@ -206,8 +208,31 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),  # Normalisation finale
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)  # Projection vers le vocabulaire
+
+        # Partage des poids entre l'embedding et la couche de sortie (weight tying)
+        # Cette technique réduit le nombre de paramètres et améliore les performances
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # Initialisation des poids 
+        self.apply(self._init_weights)
+
+
+    def _init_weights(self, module):
+
+        """Initialise les poids des modules pour que ceux si suivent une distribution normale
+        cela permet d'éviter les problèmes d'explosion ou de disparition des gradients."""
+
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'MINIGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer)**-0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
     
-    def forward(self, idx):
+    def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
 
@@ -219,7 +244,10 @@ class GPT(nn.Module):
             x = block(x)
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
-        return logits
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -287,20 +315,83 @@ class GPT(nn.Module):
 
 # --------------------------------------------------------------------------------------------------
 
-num_return_sequences = 5
-max_length = 30
-
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to('cuda')
-
-# on utilise le tokenizer gpt2 d'openai
 import tiktoken
-enc = tiktoken.get_encoding("gpt2")
+
+class DataLoaderLite:
+
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        # on utilise le tokenizer gpt2 d'openai
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(tokens)} tokens")
+        print(f"epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B,T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T+1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        self.current_position += B*T+1
+        if self.current_position + B*T+1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+# --------------------------------------------------------------------------------------------------
+
+
+
+# on essaye de détecter l'appareil sur lequel on va executer le modele 
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = 'mps'
+
+print(f"on utilise le device: {device}")
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=4, T=32)
+
+model = GPT(GPTConfig())
+model.to(device)
+
+
+# optimisation !
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x = x.to(device)
+    y = y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i} loss: {loss.item()}")
+
+
+
+import sys
+sys.exit(0)
+
+
+
 tokens = enc.encode("Hello, I'm a language model")
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to('cuda')
+x = tokens.to(device)
 
 # génération
 torch.manual_seed(42)
